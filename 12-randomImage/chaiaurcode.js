5df42ec786720c8696139ec320e050a6bfc5de07
myself-viishal <3953vishal@gmail.com>
2024-01-07 05:27:55 +0530

    11-textEditior

--- 12-randomImage/chaiaurcode.js
+++ 12-randomImage/chaiaurcode.js
@@ -1,3 +1,16 @@
-const baseURL = 'https://source.unsplash.com/all/300x300';
-
+const baseURL = "https://source.unsplash.com/all/300x300";
 // this url gives an image. Use this and NO API calls
+
+const container = document.querySelector(".content");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", (e) => {
+    if (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    console.log("working");
+    let imgElem = document.createElement("img");
+    imgElem.setAttribute("src", baseURL);
+    container.appendChild(imgElem);
+    // container.style.background = `url(${baseURL})`;
+});
