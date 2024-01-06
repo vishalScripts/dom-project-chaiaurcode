dd5a17f424568ce93e9c99ce62110ee5ad6396e3
myself-viishal <3953vishal@gmail.com>
2024-01-07 03:01:01 +0530

    Added a word

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -1,8 +1,33 @@
-const typedTextSpan = document.querySelector('.typed-text');
-const cursor = document.querySelector('.cursor');
+const typedTextSpan = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
 
-const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
+const words = ["Love", "Jhakaas", "mast", "dhinchak", "Weird", "OverPowerd"];
+let charIndex = 0;
+let wordIndex = 0;
+function type() {
+    console.table("wordIndex", wordIndex);
+    if (charIndex < words[wordIndex].length) {
+        typedTextSpan.innerHTML += words[wordIndex].charAt(charIndex);
+        // console.log(words[wordIndex]);
+        charIndex++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 1000);
+    }
+}
 
-function type() {}
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = words[wordIndex].substring(
+            0,
+            charIndex - 1
+        );
+        charIndex--;
+        setTimeout(erase, 150);
+    } else {
+        wordIndex = (wordIndex + 1) % words.length;
+        setTimeout(type, 100);
+    }
+}
 
-function erase() {}
+type();
