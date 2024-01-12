8c0b6048be06be3facf799c6cc42507be455210c
myself-viishal <3953vishal@gmail.com>
2024-01-12 14:03:54 +0530

    Project 14-cats Done ✔

--- 14-cats/chaiaurcode.js
+++ 14-cats/chaiaurcode.js
@@ -1,1 +1,23 @@
-const url = 'https://api.thecatapi.com/v1/images/search';
+const url = "https://api.thecatapi.com/v1/images/search";
+const container = document.querySelector(".container");
+
+document.querySelector(".btn").addEventListener("click", getImg);
+
+async function getImg() {
+    let response = await fetch(url);
+    let data = await response.json();
+    showImg(data[0].url);
+}
+
+function showImg(imgUrl) {
+    const imgElement = document.createElement("img");
+    imgElement.src = imgUrl;
+    imgElement.alt = "Random Cat";
+    imgElement.classList.add("random_cats");
+
+    // Remove existing images before adding a new one
+    container.innerHTML = "";
+
+    container.appendChild(imgElement);
+}
+setInterval(getImg, 2000);
