2b90f7b0bef12ed45aa127d85f1895b6bc7622a2
myself-viishal <3953vishal@gmail.com>
2024-01-13 14:33:16 +0530

    used local storage in project 15-crudDom

--- 15-crudDom/chaiaurcode.js
+++ 15-crudDom/chaiaurcode.js
@@ -4,16 +4,19 @@ const year = document.getElementById("year");
 const bookList = document.getElementById("book-list");
 const btn = document.querySelector(".btn");
 
+// Load existing data from local storage
+bookList.innerHTML = localStorage.getItem("data");
+attachDeleteListeners(); // Attach event listeners after loading existing data
+
 btn.addEventListener("click", function (e) {
     e.preventDefault();
 
     if (title.value == "" || author.value == "" || year.value == "") {
-        alert("Plese enter the input");
+        alert("Please fil the input correctly");
         return;
     }
 
     const newRow = document.createElement("section");
-
     const titleCell = document.createElement("div");
     const authorCell = document.createElement("div");
     const yearCell = document.createElement("div");
@@ -30,13 +33,28 @@ btn.addEventListener("click", function (e) {
     newRow.appendChild(yearCell);
     newRow.appendChild(dltBtn);
     bookList.appendChild(newRow);
-
-    dltBtn.addEventListener("click", (e) => {
-        let target = e.currentTarget;
-        target.parentNode.remove();
-    });
+    dltBtn.addEventListener("click", dlt);
 
     title.value = "";
     author.value = "";
     year.value = "";
+    updateLocalStorage();
 });
+
+function updateLocalStorage() {
+    localStorage.setItem("data", bookList.innerHTML);
+}
+function dlt(e) {
+    let target = e.currentTarget;
+    target.parentNode.remove();
+    updateLocalStorage();
+}
+
+function attachDeleteListeners() {
+    // Attach event listeners to delete buttons
+    // cause after refreshing the website eventListeners are deleted
+    const deleteButtons = document.querySelectorAll(".dlt-btn");
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", dlt);
+    });
+}
