a74c686547b84e1aad6fc7fe006af5350342aec4
myself-viishal <3953vishal@gmail.com>
2024-01-14 23:05:33 +0530

    Complete 16-Debouncing project

--- 16-debounce/chaiaurcode.js
+++ 16-debounce/chaiaurcode.js
@@ -1,3 +1,54 @@
 // with each character typed in search box, make an API call to randomuserme api and display a card below it. Use debounce concept to reduce api calls.
 
 // You will automatically learn about this and apply in this. ☕️
+const debounce = function (func, delay) {
+    let timeoutId;
+    return function () {
+        const context = this;
+        const args = arguments;
+
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(function () {
+            func.apply(context, args);
+        }, delay);
+    };
+};
+
+const debounceHandleInput = debounce(getData, 500);
+
+const input = document.getElementById("user-input");
+const card = document.getElementById("user-card");
+input.addEventListener("input", () => {
+    debounceHandleInput(input.value);
+});
+
+async function getData(query) {
+    try {
+        let response = await fetch(`https://randomuser.me/api/?seed=${query}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        let data = await response.json();
+        showData(data);
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+}
+
+function showData(data) {
+    const user = data.results[0];
+    console.log(user);
+
+    card.style.display = "block";
+    card.innerHTML = `
+        <img src="${user.picture.thumbnail}" alt="User Image" />
+        <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
+        <p><span class="white">Email:</span> ${user.email}</p>
+        <p><span class="white">Gender:</span> ${user.gender}</p>
+        <p><span class="white">Phone:</span> ${user.phone}</p>
+        <p><span class="white">Location:</span> ${user.location.city}, ${user.location.country}</p>
+    `;
+}
